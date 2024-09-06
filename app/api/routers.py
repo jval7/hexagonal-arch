661f53dd9258a6fe7d67812a0bd68bc5c7e443c3
Jhon Valderrama <jhonvalderramaa7@gmail.com>
2024-09-06 17:14:45 -0500

    Fix status code

--- app/api/routers.py
+++ app/api/routers.py
@@ -11,12 +11,14 @@ class DocumentInput(BaseModel):
     content: str = pydantic.Field(..., min_length=1)
 
 
-@rag_router.post("/generate-answer/")
-def generate_answer(query: str, rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
+@rag_router.post("/generate-answer/", status_code=200)
+def generate_answer(query: str,
+                    rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
     return {"answer": rag_service.generate_answer(query)}
 
 
-@rag_router.post("/save-document/")
-def save_document(document: DocumentInput, rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
+@rag_router.post("/save-document/", status_code=201)
+def save_document(document: DocumentInput,
+                  rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
     rag_service.save_document(content=document.content)
-    return {"status": "Document saved successfully"}, 201
+    return {"status": "Document saved successfully"}
